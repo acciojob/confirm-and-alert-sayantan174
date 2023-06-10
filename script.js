0756a96648a26c67f1c0397c9e2e69ca3df1637a
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-10 21:25:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+document.getElementById("ok-btn").addEventListener("click",()=>{
+	alert("You clicked OK. Proceeding...");
+});
+document.getElementById("cancel-btn").addEventListener("click",()=>{
+	alert("You clicked Cancel. Exiting...");
+})
